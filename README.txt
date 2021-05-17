86a4c4c71d70b68f61cc4992ff492f31070c5dba
jchampol <julien.champol@etu.u-bordeaux.fr>
2021-05-17 08:59:19 +0200

    Junit en bonne version, import Hamcrest impossible

--- README.txt
+++ README.txt
@@ -5,7 +5,14 @@ Remise avant le pt2.
 
 Avant le 12/05/ Pas de journalisation, réalisation du diagramme de classe.
 
-12/05/ Début de la journalisation.
+12/05 Début de la journalisation.
     Utilisation du diagramme de classe pour l'écriture de la signature de toutes les 
     méthodes et de la javadoc. Début de l'implémentation de la partie 1. Maj du diagramme
     de cas d'utilisation.
+
+16/05
+    Fin de l'implémentation de la classe PlayerMoves, mais problème de génération de tests.
+    Mp mattermost.
+
+17/05
+    Junit en version correcte, import de Hamcrest libraries impossible.
---
+++ Sokoban/test/sokoban/BoardAnalysisPackage/BoardCheckerTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokoban.BoardAnalysisPackage;
+
+import java.awt.Point;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import sokoban.BoardBuildingPackage.Board;
+import sokoban.PlayerMovesPackage.PlayerMoves;
+
+/**
+ *
+ * @author jcdru
+ */
+public class BoardCheckerTest {
+
+    public BoardCheckerTest() {
+    }
+
+    /**
+     * Test of winCheck method, of class BoardChecker.
+     */
+    @Test
+    public void testWinCheck() {
+        System.out.println("winCheck");
+        Board analysed = new Board("TestBoard", 8, 8);
+        analysed.addHorizontalWall(0, 0, 8);
+        analysed.addHorizontalWall(0, 8, 8);
+        analysed.addVerticalWall(7, 0, 8);
+        analysed.addTarget(0, 2);
+        analysed.addBox(3, 2);
+        boolean expResult = false;
+        boolean result = BoardChecker.winCheck(analysed);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of legitMove method, of class BoardChecker.
+     */
+    @Test
+    public void testLegitMove() {
+        System.out.println("legitMove");
+        Board analysed = null;
+        Point start = null;
+        Point destination = null;
+        boolean expResult = false;
+        boolean result = BoardChecker.legitMove(analysed, start, destination);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of movableBoxCheck method, of class BoardChecker.
+     */
+    @Test
+    public void testMovableBoxCheck() {
+        System.out.println("movableBoxCheck");
+        Board theBoard = null;
+        Point theBox = null;
+        PlayerMoves.Moves direction = null;
+        boolean expResult = false;
+        boolean result = BoardChecker.movableBoxCheck(theBoard, theBox, direction);
+        assertEquals(expResult, result);
+    }
+
+}
